fabdcb9f2d4de706228d18660677dd94f8e0cb2d
CeriniA <adriancerini@gmail.com>
2025-06-29 19:20:39 -0300

    otro intento mas

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TpFinalFront'  
+  base: '/TpFinalFront/'  // Agregar barra al final
 })
