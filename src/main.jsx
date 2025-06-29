39aea319c7b309c59916d7c3d27a8a91a5c38dd6
CeriniA <adriancerini@gmail.com>
2025-06-29 18:05:56 -0300

    Configurando mas cosas

--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home.jsx'
 import Products from './pages/Products.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
 import ProductDetail from './pages/ProductDetail.jsx'
-import { createBrowserRouter } from 'react-router-dom'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: '/products',
-        element: <Products />
-      },
-      {
-        path: '/about',
-        element: <About />
-      },
-      {
-        path: '/contact',
-        element: <Contact />
-      },
-      {
-        path: '/product/:id',
-        element: <ProductDetail />
-      }
-    ]
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'products/:id', element: <ProductDetail /> },
+      { path: 'about', element: <About /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+], {
+  basename: './git' // Asegúrate de que esto coincida con el nombre de tu repositorio
+})
 
-  }
-])
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </React.StrictMode>,
 )
